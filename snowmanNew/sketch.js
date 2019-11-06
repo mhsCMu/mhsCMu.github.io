3328208315fdb1934cfe5c491ea6b956cb93dda6
mhsCMu <57224179+mhsCMu@users.noreply.github.com>
2019-11-06 08:41:00 -0500

    Add files via upload

---
+++ snowmanNew/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,600);
+}
+
+function draw() {
+  // put drawing code here
+  rect (0, 369, 400, 30);
+  ellipse(200, 300, 150, 150);
+  ellipse(200, 200, 100, 100);
+  ellipse(200, 120, 75, 75);
+  line (160, 200, 74, 100);
+  line (240, 200, 324, 100);
+}
